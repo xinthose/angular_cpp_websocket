e5b5a293b7c889f49b4b122b8943250fc4b98607
xinthose <adamdunsmoor@pm.me>
2020-01-28 15:23:40 -0600

    add comments

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 import { SocketService } from "../bible.service";
 
+interface flowUpdate {
+  flowAcc: number,
+  flowRate: number,
+  flowPH: number,
+  flowConductivity: number,
+  highLevel: boolean,
+  sampleInProgress: boolean,
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -18,13 +27,20 @@ export class HomeComponent implements OnInit {
         console.debug("station closed");
       });
 
-      let data = {
-        "product_type": 1,
-        "product_type_sub_type": 2,
-        "requested_volume": 100,
-        "manifest_number": "",
-      }
-      this.socketService.sendEvent(this.socketService.event.TRANS_STARTED, data);
+    // send an event to C++
+    let data = {
+      "product_type": 1,
+      "product_type_sub_type": 2,
+      "requested_volume": 100,
+      "manifest_number": "",
+    }
+    this.socketService.sendEvent(this.socketService.event.TRANS_STARTED, data);
+
+    // receive an event with data from C++
+    this.socketService.onFlowUpdated()
+      .subscribe((data: flowUpdate) => {
+        console.debug("flow updated >> data = %O", data);
+      });
 
   }
 
